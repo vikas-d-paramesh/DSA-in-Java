192bd7225f8bddf27bfe3fbc8810b88c2b02fb70
Vikas D Paramesh <vikas.d.paramesh@gmail.com>
2026-04-12 19:02:27 +0530

    Add files via upload

---
+++ ArrayList/Sorting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Sorting{
+    public static void main(String args[]){
+        ArrayList <Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(3);
+        list.add(5);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
